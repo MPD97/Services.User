76fab2169d00d327df4c32d4a83c78ca6b89aa79
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 19:24:10 +0200

    Created Program

---
+++ src/Services.User.Api/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Convey;
+using Convey.Logging;
+using Convey.Secrets.Vault;
+using Convey.Types;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Services.User.Application;
+using Services.User.Infrastructure;
+
+namespace Services.User.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await CreateWebHostBuilder(args)
+                .Build()
+                .RunAsync();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+            => WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddApplication()
+                    .AddInfrastructure()
+                    .Build())
+                .Configure(app => app
+                    .UseInfrastructure()
+                    .UseDispatcherEndpoints(endpoints => endpoints
+                        .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
+                    ))
+                .UseLogging()
+                .UseVault();
+    }
+}
