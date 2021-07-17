d5ad0f7bc503377e959861eaafc5300b11d1ecf1
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 00:09:32 +0200

    Created UserMongoRepository

---
+++ src/Services.User.Infrastructure/Mongo/Repositories/CustomerMongoRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.User.Core.Repositories;
+using Services.User.Infrastructure.Mongo.Documents;
+
+namespace Services.User.Infrastructure.Mongo.Repositories
+{
+    public class UserMongoRepository : IUserRepository
+    {
+        private readonly IMongoRepository<UserDocument, Guid> _repository;
+
+        public UserMongoRepository(IMongoRepository<UserDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Core.Entities.User> GetAsync(Guid id)
+        {
+            var user = await _repository.GetAsync(o => o.Id == id);
+
+            return user?.AsEntity();
+        }
+
+        public Task AddAsync(Core.Entities.User user) => _repository.AddAsync(user.AsDocument());
+        public Task UpdateAsync(Core.Entities.User user) => _repository.UpdateAsync(user.AsDocument());
+    }
+}
