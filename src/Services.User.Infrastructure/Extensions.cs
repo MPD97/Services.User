c342c4165b75a919ddb1499f7356daa5f1c8901b
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 19:23:49 +0200

    Created extensions

---
+++ src/Services.User.Infrastructure/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Convey;
+using Microsoft.AspNetCore.Builder;
+
+namespace Services.User.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
+        {
+            return builder;
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            return app;
+        }
+
+    } 
+}
