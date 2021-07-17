003edbd0d3d78de02b5298ce43d4e01842438d53
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 23:12:22 +0200

    Updated Extensions

--- src/Services.User.Infrastructure/Extensions.cs
+++ src/Services.User.Infrastructure/Extensions.cs
@@ -1,5 +1,12 @@
-﻿using Convey;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Convey;
+using Convey.MessageBrokers;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Services.User.Infrastructure.Contexts;
 
 namespace Services.User.Infrastructure
 {
@@ -15,5 +22,46 @@ namespace Services.User.Infrastructure
             return app;
         }
 
-    } 
+        internal static CorrelationContext GetCorrelationContext(this IHttpContextAccessor accessor)
+        {
+            var headers = accessor.HttpContext?.Request.Headers;
+            if (headers != null && headers.TryGetValue("Correlation-Context", out var json))
+            {
+                return JsonConvert.DeserializeObject<CorrelationContext>(json.FirstOrDefault());
+            }
+
+            return null;
+        }
+        
+        internal static IDictionary<string, object> GetHeadersToForward(this IMessageProperties messageProperties)
+        {
+            const string sagaHeader = "Saga";
+            if (messageProperties?.Headers is null || !messageProperties.Headers.TryGetValue(sagaHeader, out var saga))
+            {
+                return null;
+            }
+
+            return saga is null
+                ? null
+                : new Dictionary<string, object>
+                {
+                    [sagaHeader] = saga
+                };
+        }
+        
+        internal static string GetSpanContext(this IMessageProperties messageProperties, string header)
+        {
+            if (messageProperties is null)
+            {
+                return string.Empty;
+            }
+
+            if (messageProperties.Headers.TryGetValue(header, out var span) && span is byte[] spanBytes)
+            {
+                return Encoding.UTF8.GetString(spanBytes);
+            }
+
+            return string.Empty;
+        }
+    }
 }
