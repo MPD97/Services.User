86785ddd8a819ccefbcd0075794573be9fcfbf3e
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 23:11:58 +0200

    Created services

---
+++ src/Services.User.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Services.User.Application.Services;
+
+namespace Services.User.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {        
+        public DateTime Now  => DateTime.UtcNow;
+    }
+}
---
+++ src/Services.User.Infrastructure/Services/EventMapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Convey.CQRS.Events;
+using Services.User.Application.Services;
+using Services.User.Core;
+using Services.User.Core.Events;
+
+namespace Services.User.Infrastructure.Services
+{
+    public class EventMapper : IEventMapper
+    {
+        public IEnumerable<IEvent> MapAll(IEnumerable<IDomainEvent> events)
+            => events.Select(Map);
+
+        public IEvent Map(IDomainEvent @event)
+        {
+            switch (@event)
+            {
+                case UserRegistrationCompleted e: return new Application.Events.UserCreated(e.User.Id);
+                case UserStateChanged e:
+                    return new Application.Events.UserStateChanged(e.User.Id,
+                        e.User.State.ToString().ToLowerInvariant(), e.PreviousState.ToString().ToLowerInvariant());
+            }
+
+            return null;
+        }
+    }
+}
