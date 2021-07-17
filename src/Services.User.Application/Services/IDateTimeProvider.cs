11e60750720f32d509c85c9ec70172fa00bd9d9b
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 22:29:30 +0200

    Creates services

---
+++ src/Services.User.Application/Services/IDateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Services.User.Application.Services
+{
+    public interface IDateTimeProvider
+    {
+        DateTime Now { get; }
+    }
+}
---
+++ src/Services.User.Application/Services/IEventMapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Convey.CQRS.Events;
+using Services.User.Core;
+
+namespace Services.User.Application.Services
+{
+    public interface IEventMapper
+    {
+        IEvent Map(IDomainEvent @event);
+        IEnumerable<IEvent> MapAll(IEnumerable<IDomainEvent> events);
+    }
+}
