1c9a2db18c5004a4e435027c30ff635f56e97c1b
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 19:28:00 +0200

    Created InvalidAggregateIdException

---
+++ src/Services.User.Core/Exceptions/InvalidAggregateIdException.cs
@@ -0,0 +1,11 @@
+namespace Services.User.Core.Exceptions
+{
+    public class InvalidAggregateIdException : DomainException
+    {
+        public override string Code => "invalid_aggregate_id";
+        
+        public InvalidAggregateIdException() : base($"Invalid aggregate id.")
+        {
+        }
+    }
+}
