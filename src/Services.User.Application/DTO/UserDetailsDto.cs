bde35d00100d0bf9c8d66f7d36266c08215a965c
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 22:47:34 +0200

    Created DTOs

---
+++ src/Services.User.Application/DTO/UserDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services.User.Application.DTO
+{
+    public class UserDetailsDto : UserDto
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public IEnumerable<Guid> CompletedRuns { get; set; }
+    }
+}
---
+++ src/Services.User.Application/DTO/UserStateDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Services.User.Application.DTO
+{
+    public class UserStateDto
+    {
+        public Guid Id { get; set; }
+        public string State { get; set; }
+    }
+}
