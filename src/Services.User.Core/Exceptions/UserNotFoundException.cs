a936700ec7ef2798ccf7c75776648a883596e9e9
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-17 22:30:48 +0200

    Created exception

---
+++ src/Services.User.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Services.User.Core.Exceptions
+{
+    public class UserNotFoundException : DomainException
+    {
+        public override string Code { get; } = "user_not_found";
+        public Guid Id { get; }
+
+        public UserNotFoundException(Guid id) : base($"User with id: {id} was not found.")
+        {
+            Id = id;
+        }
+    }
+}
