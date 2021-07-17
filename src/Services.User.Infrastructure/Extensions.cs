6c31ef54ae81cc5e9a8f974fbace13eadcae9051
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 00:37:34 +0200

    Update extensions

--- src/Services.User.Infrastructure/Extensions.cs
+++ src/Services.User.Infrastructure/Extensions.cs
@@ -1,12 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Convey;
+using Convey.WebApi;
+using Convey.WebApi.Security;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
+using Convey.CQRS.Queries;
+using Convey.Discovery.Consul;
+using Convey.Docs.Swagger;
+using Convey.HTTP;
+using Convey.LoadBalancing.Fabio;
 using Convey.MessageBrokers;
+using Convey.MessageBrokers.CQRS;
+using Convey.MessageBrokers.Outbox;
+using Convey.MessageBrokers.Outbox.Mongo;
+using Convey.MessageBrokers.RabbitMQ;
+using Convey.Metrics.AppMetrics;
+using Convey.Persistence.MongoDB;
+using Convey.Persistence.Redis;
+using Convey.Security;
+using Convey.Tracing.Jaeger;
+using Convey.Tracing.Jaeger.RabbitMQ;
+using Convey.WebApi.CQRS;
+using Convey.WebApi.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
+using Services.User.Application;
+using Services.User.Application.Commands;
+using Services.User.Application.Events.External;
+using Services.User.Application.Services;
+using Services.User.Core.Repositories;
+using Services.User.Infrastructure.Exceptions;
 using Services.User.Infrastructure.Contexts;
+using Services.User.Infrastructure.Decorators;
+using Services.User.Infrastructure.Logging;
+using Services.User.Infrastructure.Mongo.Documents;
+using Services.User.Infrastructure.Mongo.Repositories;
+using Services.User.Infrastructure.Services;
 
 namespace Services.User.Infrastructure
 {
@@ -14,11 +48,51 @@ namespace Services.User.Infrastructure
     {
         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
         {
-            return builder;
+            builder.Services.AddSingleton<IEventMapper, EventMapper>();
+            builder.Services.AddTransient<IMessageBroker, MessageBroker>();
+            builder.Services.AddTransient<IUserRepository, UserMongoRepository>();
+            builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+            builder.Services.AddTransient<IAppContextFactory, AppContextFactory>();
+            builder.Services.AddTransient(ctx => ctx.GetRequiredService<IAppContextFactory>().Create());
+            builder.Services.TryDecorate(typeof(ICommandHandler<>), typeof(OutboxCommandHandlerDecorator<>));
+            builder.Services.TryDecorate(typeof(IEventHandler<>), typeof(OutboxEventHandlerDecorator<>));
+
+            return builder
+                .AddErrorHandler<ExceptionToResponseMapper>()
+                .AddQueryHandlers()
+                .AddInMemoryQueryDispatcher()
+                .AddHttpClient()
+                .AddConsul()
+                .AddFabio()
+                .AddRabbitMq(plugins: p => p.AddJaegerRabbitMqPlugin())
+                .AddMessageOutbox(o => o.AddMongo())
+                .AddExceptionToMessageMapper<ExceptionToMessageMapper>()
+                .AddMongo()
+                .AddRedis()
+                .AddMetrics()
+                .AddJaeger()
+                .AddHandlersLogging()
+                .AddMongoRepository<UserDocument, Guid>("users")
+                .AddWebApiSwaggerDocs()
+                .AddCertificateAuthentication()
+                .AddSecurity();
         }
 
         public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
         {
+            app.UseErrorHandler()
+                .UseSwaggerDocs()
+                .UseJaeger()
+                .UseConvey()
+                .UsePublicContracts<ContractAttribute>()
+                .UseMetrics()
+                .UseCertificateAuthentication()
+                .UseRabbitMq()
+                .SubscribeCommand<CompleteUserRegistration>()
+                .SubscribeCommand<ChangeUserState>()
+                .SubscribeEvent<SignedUp>()
+                .SubscribeEvent<RunCompleted>();
+
             return app;
         }
 
