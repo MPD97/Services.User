11a1ce4f172151d3fe8ce0413fbdcd2056d325e7
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 00:04:25 +0200

    Created handlers

---
+++ src/Services.User.Infrastructure/Mongo/Queries/Handlers/GetUserStateHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.User.Application.DTO;
+using Services.User.Application.Queries;
+using Services.User.Infrastructure.Mongo.Documents;
+
+namespace Services.User.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetUserStateHandler : IQueryHandler<GetUserState, UserStateDto>
+    {
+        private readonly IMongoRepository<UserDocument, Guid> _userRepository;
+
+        public GetUserStateHandler(IMongoRepository<UserDocument, Guid> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserStateDto> HandleAsync(GetUserState query)
+        {
+            var document = await _userRepository.GetAsync(p => p.Id == query.UserId);
+
+            return document is null
+                ? null
+                : new UserStateDto
+                {
+                    Id = document.Id,
+                    State = document.State.ToString().ToLowerInvariant()
+                };
+        }
+    }
+}
---
+++ src/Services.User.Infrastructure/Mongo/Queries/Handlers/GetUsersHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using Services.User.Application.DTO;
+using Services.User.Application.Queries;
+using Services.User.Infrastructure.Mongo.Documents;
+
+namespace Services.User.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetUsersHandler : IQueryHandler<GetUsers, IEnumerable<UserDto>>
+    {
+        private readonly IMongoRepository<UserDocument, Guid> _userRepository;
+
+        public GetUsersHandler(IMongoRepository<UserDocument, Guid> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<UserDto>> HandleAsync(GetUsers query)
+        {
+            var customers = await _userRepository.FindAsync(_ => true);
+
+            return customers.Select(p => Documents.Extensions.AsDto((UserDocument) p));
+        }
+    }
+}
