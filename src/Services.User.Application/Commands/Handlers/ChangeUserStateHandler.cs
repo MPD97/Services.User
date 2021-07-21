fbb8adf42677ef09f6ba1510b7c9217d18064fa5
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-21 11:34:13 +0200

    Updated commands

--- src/Services.User.Application/Commands/Handlers/ChangeUserStateHandler.cs
+++ src/Services.User.Application/Commands/Handlers/ChangeUserStateHandler.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Convey.CQRS.Commands;
-using Services.User.Application.Exceptions;
 using Services.User.Application.Services;
 using Services.User.Core.Entities;
 using Services.User.Core.Exceptions;
@@ -61,40 +60,6 @@ namespace Services.User.Application.Commands.Handlers
             }
 
             await _userRepository.UpdateAsync(user);
-            var events = _eventMapper.MapAll(user.Events);
-            await _messageBroker.PublishAsync(events.ToArray());
-        }
-    }
-    public class CompleteUserRegistrationHandler : ICommandHandler<CompleteUserRegistration>
-    {
-        private readonly IUserRepository _userRepository;
-        private readonly IEventMapper _eventMapper;
-        private readonly IMessageBroker _messageBroker;
-        
-        public CompleteUserRegistrationHandler(IUserRepository userRepository, IEventMapper eventMapper,
-            IMessageBroker messageBroker)
-        {
-            _userRepository = userRepository;
-            _eventMapper = eventMapper;
-            _messageBroker = messageBroker;
-        }
-
-        public async Task HandleAsync(CompleteUserRegistration command)
-        {
-            var user = await _userRepository.GetAsync(command.UserId);
-            if (user is null)
-            {
-                throw new UserNotFoundException(command.UserId);
-            }
-            
-            if (user.State is State.Valid)
-            {
-                throw new UserAlreadyRegisteredException(command.UserId);
-            }
-
-            user.CompleteRegistration(command.FullName, command.Address);
-            await _userRepository.UpdateAsync(user);
-
             var events = _eventMapper.MapAll(user.Events);
             await _messageBroker.PublishAsync(events.ToArray());
         }
