d4865c24c76c157b273a8ef4cdc819ce8ce86fe4
MPD97 <mateuszpracadom@gmail.com>
2021-07-26 13:04:10 +0200

    Updated dto

--- src/Services.User.Application/DTO/UserDetailsDto.cs
+++ src/Services.User.Application/DTO/UserDetailsDto.cs
@@ -6,8 +6,7 @@ namespace Services.User.Application.DTO
     public class UserDetailsDto : UserDto
     {
         public string Email { get; set; }
-        public string FullName { get; set; }
-        public string Address { get; set; }
+        public string Pseudonym { get; set; }
         public IEnumerable<Guid> CompletedRuns { get; set; }
     }
 }
