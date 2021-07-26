513cab5668252d5b41376e4b4d414c641de3bb28
MPD97 <mateuszpracadom@gmail.com>
2021-07-26 12:58:09 +0200

    Created exception

---
+++ src/Services.User.Core/Exceptions/InvalidUserPseudonymLengthException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Services.User.Core.Exceptions
+{
+    public class InvalidUserPseudonymLengthException : DomainException
+    {
+        public override string Code { get; } = "invalid_user_pseudonym_length";
+        public Guid UserId { get; }
+        public int PseudonymLength { get; }
+        public int MinLength { get; }
+        public int MaxLength { get; }
+
+        public InvalidUserPseudonymLengthException(Guid userId, int pseudonymLength, int minLength, int maxLength) 
+            : base($"User with id: {userId} has invalid pseudonym length: {pseudonymLength}. " +
+                   $"Pseudonym length should be between: {minLength} and {maxLength} characters long.")
+        {
+            UserId = userId;
+            PseudonymLength = pseudonymLength;
+            MinLength = minLength;
+            MaxLength = maxLength;
+        }
+    }
+}
